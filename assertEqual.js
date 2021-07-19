67c059cd7d9a97f579da625e71de451c460e6dc8
Drew Kang <kangdrew95@gmail.com>
2021-07-19 21:01:18 +0000

    Completed the first Lotide assignment section

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + "Assertion Passed: " + actual.toString()
+    + " === " + expected.toString());
+  } else {
+    console.log("🛑🛑🛑 " + "Assertion Failed: " + actual.toString()
+    + " !== " + expected.toString());
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
