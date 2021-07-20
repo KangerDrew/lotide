255b124aff498a52246c5d295963eb3828f6cb9f
Drew Kang <kangdrew95@gmail.com>
2021-07-20 23:59:00 +0000

    Added new file, eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(ar1, ar2) {
+  if (ar1.length === ar2.length) {
+    for (let i = 0; i < ar1.length; i ++) {
+      if (ar1[i] !== ar2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Testing
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
