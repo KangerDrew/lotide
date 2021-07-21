fbc2814a6c124814f375d4e6fa3c4a6aa5bfbdbd
Drew Kang <kangdrew95@gmail.com>
2021-07-21 01:49:32 +0000

    Added new file, flatten.js

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  let newAr = [];
+  for (let stuff of array) {
+    if (Array.isArray(stuff)) {
+      for (let el of stuff) {
+        newAr.push(el);
+      }
+    } else {
+      newAr.push(stuff);
+    }
+  }
+  return newAr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
