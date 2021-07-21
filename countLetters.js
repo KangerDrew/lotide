e645554f5dfeacac6ad79989bfebc6a8561408cf
Drew Kang <kangdrew95@gmail.com>
2021-07-21 21:07:22 +0000

    Added a new file, countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const letters = {};
+
+  for (const l of string) {
+    if (letters[l]) {
+      letters[l] += 1;
+    } else {
+      letters[l] = 1;
+    }
+  }
+
+  return letters;
+};
+
+assertEqual(countLetters("touch")['t'], 1)
+assertEqual(countLetters("loseyourselftodance")['o'], 3)
+assertEqual(countLetters("instantcrush")['s'], 2)
