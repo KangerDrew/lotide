ce752b592bfe0e06263a7982ad570a0f1a6a68c3
Drew Kang <kangdrew95@gmail.com>
2021-07-22 20:50:44 +0000

    Added another file, takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const eqArrays = function(ar1, ar2) {
+  if (ar1.length === ar2.length) {
+    for (let i = 0; i < ar1.length; i ++) {
+      if (ar1[i] !== ar2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+  let st = eqArrays(ar1,ar2);
+  if (st === true) {
+    console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${ar1} !== ${ar2}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const result = [];
+
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1,2,5,7,2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"])
