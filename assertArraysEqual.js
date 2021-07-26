75e3921cc01b889835b45f43e3a70c1c5063feca
Drew Kang <kangdrew95@gmail.com>
2021-07-26 22:06:00 +0000

    Refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(ar1, ar2) {
-  if (ar1.length === ar2.length) {
-    for (let i = 0; i < ar1.length; i ++) {
-      if (ar1[i] !== ar2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(ar1, ar2) {
   let st = eqArrays(ar1,ar2);
@@ -20,6 +9,4 @@ const assertArraysEqual = function(ar1, ar2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [4, 5, 6]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
